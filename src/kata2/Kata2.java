4203f3187adfe17af52f112b8dca648a4d7ed275
Familia <Familia@DESKTOP-U4D87FT>
2020-01-10 10:54:39 +0200

    version 2 second draft

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -7,12 +7,11 @@ public class Kata2 {
 
     public static void main(String[] args) {
         int data [] = {1,2,2,3,3,3,4,4,4,4};
-        Map histogram = new HashMap<Integer,Integer>();
+        Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
         for (int key : data) {
-            System.out.println("Histograma: " + histogram.get(key) + " " + histogram.containsKey(key));
-            System.out.println("Array "+ key );
-            System.out.println("");
-            histogram.put(key, histogram.containsKey(key) ? histogram.get(key).hashCode() + 1 : 1);
+            
+            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
+            
         }
         
         System.out.println(histogram);
